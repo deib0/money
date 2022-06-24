113283d022f811c84709f80f161035b2249c77f3
deibo <deib0@foxmail.com>
2022-06-24 16:07:12 +0800

    完成存储LocalStorage

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -14,12 +14,13 @@ type Record={
   type:string
   amount:number
 }
+const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
   import NumberPad from '@/components/Money/NumberPad.vue';
   import Types from '@/components/Money/Types.vue';
   import Notes from '@/components/Money/Notes.vue';
   import Tags from '@/components/Money/Tags.vue';
 import Vue from 'vue';
-import { Component } from 'vue-property-decorator';
+import { Component,Watch } from 'vue-property-decorator';
 @Component({components:{Tags,NumberPad,Types,Notes}})
 export default class Money extends Vue {
   tags=['衣','食','住','行','电动'];
@@ -29,6 +30,7 @@ export default class Money extends Vue {
   type:'-',
   amount:0,
   };
+  recordList:Record[]=recordList
   onUpdateTags(tags:string[]){
     this.record.tags=tags
   };
@@ -36,8 +38,13 @@ export default class Money extends Vue {
 this.record.notes=notes
   };
   onSaveRecord(){
-    console.log(this.record);
+      const record2: Record = JSON.parse(JSON.stringify(this.record));//  深拷贝
+      this.recordList.push(record2);
   }
+  @Watch('recordList')
+      onRecordListChange() {
+      window.localStorage.setItem('recordList', JSON.stringify(this.recordList));
+    }
 }
 </script>
 
