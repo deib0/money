c6179adb7381cf0f2e7d35569b365b50a42d348a
deibo <deib0@foxmail.com>
2022-06-29 15:58:40 +0800

    完成统计页面的功能

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -3,14 +3,14 @@
         <Tabs class="money"
         :types="moneyTypes" :selectedType.sync="moneySelectedType" />        
         <ol>
-            <li v-for="(value,name) in result" :key="name"> 
-            <h3 class="title">{{name}}</h3> 
+            <li v-for="(u,index) in result" :key="index"> 
+            <h3 class="title">{{showDay(u.title)}}<span>￥{{showTotal(u.items)}}</span></h3> 
             <ol>
-                <li v-for="(u,index) in value" :key="index" 
+                <li v-for="(u2,index2) in u.items" :key="index2" 
                 class="record">
-                <span>{{u.selectedTags}}</span>
-                <span>{{u.notes}}</span>
-                <span>￥{{u.amount}}</span>
+                <span>{{showTags(u2.selectedTags)}}</span>
+                <span>{{u2.notes}}</span>
+                <span>￥{{u2.amount}}</span>
                 </li>
             </ol>
             </li>
@@ -23,23 +23,57 @@ import Vue from "vue";
 import { Component } from 'vue-property-decorator'
 import Tabs from '../components/Tabs.vue';
 import Layout from "@/components/Layout.vue";
+import dayjs from 'dayjs';
 @Component({components:{Tabs,Layout}})
 export default class Statistics extends Vue {
     moneyTypes=['支出','收入']
     moneySelectedType='支出'
 hashTable={}
-get recordList(){return this.$store.state.recordList}
+get recordList(){return (this.$store.state as RootState).recordList}
 get result(){
-    const hashTable:{[key:string]:RecordItem[]} = {}
-    const {recordList}=this
-    for(let i=1; i<recordList.length;i++){
-        let [day,time] = recordList[i].date.split('T')
-        hashTable[day]=hashTable[day]||[]
-        hashTable[day].push(recordList[i])
+        const {recordList}=this
+        const recordList2 = recordList.filter((item)=>item.type===this.moneySelectedType)
+        .sort((a, b) => dayjs(b.date).valueOf() - dayjs(a.date).valueOf());
+    type HashTable ={title:string,total?:number,items:RecordItem[]}[]
+    const hashTable:HashTable=[{title:dayjs(recordList2[0].date).format('YYYY-MM-DD'),items:[recordList2[0]]}]
+    for(let i=1; i<recordList2.length;i++){
+        const current =recordList2[i]
+        const last = hashTable[hashTable.length-1]
+        if(dayjs(last.title).isSame(dayjs(current.date),'day')){
+            last.items.push(current)
+        }else{
+            hashTable.push({title:dayjs(current.date).format('YYYY-MM-DD'),items:[current]})
+        }
     }
     return hashTable
 
 }
+showTags(tags:string[]){
+    let string ='无'
+    if(tags.length>0){
+     string=tags.join(',')
+    }
+    return string
+}
+showDay(date:string|number){
+    let now = dayjs()
+    if(dayjs(date).isSame(now,'day')){
+        return '今天'
+    }else if(dayjs(date).isSame(now.subtract(1,'day'),'day')){
+        return '昨天'
+    }else if(dayjs(date).isSame(now.subtract(2,'day'),'day')){
+        return '前天'
+    }
+    else{
+        return date
+    }
+}
+showTotal(items:RecordItem[]){
+    let amount = 0
+    items.map(item=>amount+=item.amount)
+    return amount
+}
+
 created(){
     this.$store.commit('fetchRecordList')
 }
