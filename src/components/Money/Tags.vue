8f3986a86164abf6c5b40313be4b186a445b9720
deibo <deib0@foxmail.com>
2022-06-30 15:41:41 +0800

    修改了记账页面的icon样式

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,112 +1,110 @@
 <template>
   <div class="tags">
-    <ul class="current"> 
-      <li v-for="u in tags" :key="u.tagName" 
-      @click="toggle(u.tagName)" 
-      :class="selectedTags.indexOf(u.tagName)>=0?'selected':''" >
-      <div class="icon">
-        <Iconfont name="money"  class="iconfont"/>
-      </div>
-        <span class="tagname">{{u.tagName}}</span></li>
+    <ul class="current">
+      <li
+        v-for="u in tags"
+        :key="u.tagName"
+        @click="toggle(u.tagName)"
+        :class="selectedTags.indexOf(u.tagName) >= 0 ? 'selected' : ''"
+      >
+        <div class="icon">
+          <Iconfont name="money" class="iconfont" />
+        </div>
+        <p class="tagname">{{ u.tagName }}</p>
+      </li>
+      <li>
+        <button @click="createTag" class="new">
+          <div class="icon">
+            <Iconfont name="add" class="iconfont" />
+          </div>
+          <p class="tagname">新增标签</p>
+        </button>
+      </li>
     </ul>
-      <div class="new">
-        <button @click="createTag">
-      <div class="icon">
-        <Iconfont name="money"  class="iconfont"/>
-      </div>
-      <span class="tagname">新增标签</span>
-      </button>
-    </div>
   </div>
-
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import {Component,Prop} from 'vue-property-decorator'
+import { Component, Prop } from "vue-property-decorator";
 @Component
 export default class Tags extends Vue {
-  @Prop() tags: Tag[]|undefined
-  selectedTags:string[]=[]
-  createTag(){
-    const tagName=prompt('请输入标签名')
-    if(tagName===''){return}
-    this.$emit('update:newTag',tagName)
-  }
-  toggle(tagName:string){
-    let index=this.selectedTags.indexOf(tagName)
-    if(index>=0){
-      this.selectedTags.splice(index,1)
-    }else{
-      this.selectedTags.push(tagName)
+  @Prop() tags: Tag[] | undefined;
+  selectedTags: string[] = [];
+  createTag() {
+    const tagName = prompt("请输入标签名");
+    if (tagName === "") {
+      return;
     }
-    this.$emit('update:selectedTag',this.selectedTags)
-  } 
+    this.$emit("update:newTag", tagName);
+  }
+  toggle(tagName: string) {
+    let index = this.selectedTags.indexOf(tagName);
+    if (index >= 0) {
+      this.selectedTags.splice(index, 1);
+    } else {
+      this.selectedTags.push(tagName);
+    }
+    this.$emit("update:selectedTag", this.selectedTags);
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-  .tags {
-    border: 1px solid #000;
-    display: flex;
-    padding: 16px;
-    flex-grow: 1;
+.tags {
+  padding: 16px;
+  flex-grow: 1;
+  > .current {
     display: flex;
     flex-direction: row;
+    justify-content: start;
+    align-items: flex-start;
     flex-wrap: wrap;
-    > .current {
+    > li {
       display: flex;
-      flex-wrap: wrap;
-      > li {
+      flex-direction: column;
+      align-items: center;
+      height: 70px;
+      width: 20%;
+      margin-top: 4px;
+      overflow: hidden;
+      .icon {
         display: flex;
-        flex-direction: column;
+        flex-direction: row;
+        justify-content: center;
         align-items: center;
-        height: 70px;
-        width: 60px;
-        text-align: center;
-        margin-right: 12px;
-        margin-top: 4px;
-        &.selected .icon{
-          background: #a4d6c8;
-        }
-        &.selected .tagname{
-          color: #000000;
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        background: #f4f4f4;
+        .iconfont {
+          width: 30px;
+          height: 30px;
         }
       }
-    }
-    .icon {
-          .iconfont {
-            width: 30px;
-            height: 30px;
-          }
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          width: 40px;
-          height: 40px;
-          border-radius: 50%;
-          background: #f4f4f4;
-        }
         .tagname {
-      line-height: 30px;
-      font-size: 14px;
-      font-weight: 700;
-      overflow: hidden;
-      color: #999999;
-    }
-    > .new {
-      button {
-        background: #f4f4f4;
+        text-align: center;
+        width: 100%;
+        height: 30px;
+        line-height: 30px;
+        font-size: 14px;
+        font-weight: 700;
+        color: #999999;
+      }
+      .new {
         display: flex;
         flex-direction: column;
-        justify-content: start;
         align-items: center;
-        margin-top: 4px;
-        height: 70px;
-        width: 60px;
         border: none;
+        background: #f4f4f4;
+      }
+      &.selected .icon {
+        background: #a4d6c8;
+      }
+      &.selected .tagname {
+        color: #000000;
       }
     }
   }
+}
 </style>
