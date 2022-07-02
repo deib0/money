2eb225eb1bbdaeae1588adecbbf9cd5f20da0e2b
deibo <deib0@foxmail.com>
2022-07-02 23:15:40 +0800

    部署到github

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
- git push -f git@gitee.com:dangxiadeliliang/money-website.git master:gh-pages
-# git push -f git@github.com:deib0/money-website.git master:gh-pages
+# git push -f git@gitee.com:dangxiadeliliang/money-website.git master:gh-pages
+ git push -f git@github.com:deib0/money-website.git master:gh-pages
 cd -
